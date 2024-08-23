7f9207a9c2df7e3fcfbdc690962ee0e27aba4a27
Subha1407 <001saini.subha@gmail.com>
2024-08-23 20:05:51 +0530

    starter ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import { Route, Routes } from 'react-router-dom'
 import Homepage from './pages/Homepage'
+import Pricing from './pages/Pricing'
+import Product from './pages/Product'
+import Login from './pages/Login'
+import {AppLayout} from './pages/AppLayout'
 import './App.css'
 
 function App() {
@@ -7,6 +11,10 @@ function App() {
   return (
     <Routes>
       <Route path='/' element={<Homepage/>}></Route>
+      <Route path='/app' element={<AppLayout/>}></Route>
+      <Route path='/pricing' element={<Pricing/>}></Route>
+      <Route path='/product' element={<Product/>}></Route>
+      <Route path='/login' element={<Login/>}></Route>
     </Routes>
   )
 }
